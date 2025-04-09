a3052588eb3c7fb775fc16d96466c7f583e0ce2f
CaNucNuong <100041034+Binh707@users.noreply.github.com>
2025-04-09 11:27:46 +0700

    Update train_mem.py

--- graphgpt/train/train_mem.py
+++ graphgpt/train/train_mem.py
@@ -1,6 +1,8 @@
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 
 # Need to call this before importing transformers.
+# import sys
+# sys.path.insert(0, "/content/GraphGPT")
 from graphgpt.train.llama_flash_attn_monkey_patch import (
     replace_llama_attn_with_flash_attn,
 )
